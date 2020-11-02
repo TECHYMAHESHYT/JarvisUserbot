153aca24b4ab40ec3f1fa0f0b43f94f0221d40c1
TECHYMAHESHYT <57401105+TECHYMAHESHYT@users.noreply.github.com>
2020-11-02 12:43:37 +0530

    Update __init__.py

--- jarvis/plugins/__init__.py
+++ jarvis/plugins/__init__.py
@@ -44,7 +44,7 @@ else:
     dbstats = "Not Fine ❌"
 
 inlinestats = (
-    f"✘ SHOWING JARVIS STATS ✘\n"
+    f"✘ SHOWING Techy's STATS ✘\n"
     f"VERSION = {currentversion} \n"
     f"DATABASE = {dbstats} \n"
     f"SUDO = {ssudo} \n"
